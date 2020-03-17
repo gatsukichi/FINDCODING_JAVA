b81711d2d96daa2d06c9967177eb77e848f73c45
katsukichi <52203107+gatsukichi@users.noreply.github.com>
2020-03-17 17:03:52 +0900

    5일차 과제

--- PigSave2.java
+++ PigSave2.java
@@ -1,5 +1,12 @@
 package ssk01;
 
 public class PigSave2 {
-
+	private int total=0;
+	
+	public int getTotal(){
+		return total;
+	}
+	public void deposit(int amount) {
+		total+=amount;
+	}
 }
--- PigSaveEx2.java
+++ PigSaveEx2.java
@@ -1,5 +1,33 @@
 package ssk01;
 
-public class PigSaveEx2 {
+import java.util.*;
 
+public class PigSaveEx2 {
+	public static void main(String[] args) {
+		PigSave2 PS = new PigSave2();
+		Scanner s = new Scanner(System.in);
+		int userInput,selection;
+		
+		while(true) {
+			System.out.print("업무를 선택하시오(1:입금,2:잔액확인,3:종료) : ");
+			selection = s.nextInt();
+			if(selection==1){
+				System.out.print("입금할 금액을 입력하시오 : ");
+				userInput=s.nextInt();
+				PS.deposit(userInput);
+				System.out.println("입금 완료");
+			}else if(selection==2) {
+				System.out.println("현재 잔액 : " + PS.getTotal());
+			}else if(selection==3) {
+				break;
+			}else {
+				System.out.println("잘못 입력하셨습니다.");
+			}
+			
+		}
+		
+		
+		System.out.println("은행업무 종료");
+		s.close();
+	}
 }
